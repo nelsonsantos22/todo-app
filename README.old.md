3f503fe8a204aa160a0850621ce78cbb6cf625a7
nelsonsantos <nelsonsilvasantos@outlook.com>
2020-09-29 12:29:24 +0100

    add hard-coded checklist items

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# todo-app
+A simple todo app using React with the purpose of studing it.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p><input type="checkbox"/>Task 1</p>
+      <p><input type="checkbox"/>Task 2</p>
+      <p><input type="checkbox"/>Task 3</p>
+      <p><input type="checkbox"/>Task 4</p>
+    </div>
+  )
+}
+
+export default App
